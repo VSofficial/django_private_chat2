33093d0b7762e536b0003b05e0fb14af8517d19e
Varun Sharma <vsofficialmail@gmail.com>
2023-07-19 00:43:25 +0530

    iOS integration

---
+++ example-iOS/example-iOS/example_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  example_iOSApp.swift
+//  example-iOS
+//
+//  Created by Varun Sharma on 18/07/23.
+//
+
+import SwiftUI
+
+@main
+struct example_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
